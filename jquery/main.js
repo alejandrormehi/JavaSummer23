51e787330907466369c7b9ee3a64b2ba4d37e954
Ale <alejo1996.96@gmail.com>
2023-06-20 19:53:24 +0200

    tercer3eee

--- jquery/main.js
+++ jquery/main.js
@@ -5,16 +5,27 @@ let arrVacia =[];
 let currentIndex = 0;
 
  function pushArray(){
-    if(currentIndex < array.length){
+    if(arrVacia.length >= array.length){
+        document.getElementById('push').disabled=true;
+    }
+    else if(currentIndex < array.length){
     arrVacia.push(array[currentIndex]);
     currentIndex = (currentIndex + 1) % array.length;
 
     const p = document.getElementById('result');
     p.innerHTML = arrVacia.join(' ');
 
-    if(arrVacia.length === array.length){
-        document.getElementById('push').disabled=true;
-    }
+    
   }
 }
+
+const unshift = document.getElementById('unshift');
+
+   
+    unshift.onclick = function(){
+    let randomVal = array[Math.floor(array.length*Math.random())];
+    arrVacia.unshift(randomVal);
+    let p = document.getElementById('result');
+    p.innerHTML = arrVacia.join(' ');
+    }
 
